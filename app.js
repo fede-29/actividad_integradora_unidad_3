f8590f8bc609b2ff80c88ff91d470ec8b6f93b34
fede-29 <fede18_01@hotmail.com>
2026-06-19 20:57:42 -0300

    se realizo el paso 2 (separación app de server, y actualizacion script)

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 const express = require('express');
-
 const app = express();
 
 app.use(express.json());
 
 const tareasRoutes = require('./routes/tareas.routes');
-
 app.use('/tareas', tareasRoutes);
 
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    console.log(`Servidor ejecutándose en puerto ${PORT}`);
-});
+module.exports = app;
